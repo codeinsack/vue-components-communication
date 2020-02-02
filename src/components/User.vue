5f3e92e7625c7828ef0b47b2f55a727923d96971
codeinsack <codeinsack@gmail.com>
2020-02-02 12:59:20 +0300

    implement event bus

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,10 +6,17 @@
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <app-user-detail :name="name"></app-user-detail>
+        <app-user-detail
+          :userAge="age"
+          :name="name"
+          @nameWasReset="name = $event"
+        ></app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit
+          :userAge="age"
+          @ageWasEdited="age = $event"
+        ></app-user-edit>
       </div>
     </div>
   </div>
@@ -23,6 +30,7 @@
     data() {
       return {
         name: 'Max',
+        age: 27,
       }
     },
     methods: {
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,21 +3,35 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User name: {{ switchName() }}</p>
+    <p>User age: {{ userAge }}</p>
+    <button @click="resetName">Reset</button>
   </div>
 </template>
 
 <script>
+  import { eventBus } from '../main'
+
   export default {
     props: {
       name: {
         type: String,
         default: 'Melanie',
-      }
+      },
+      userAge: Number,
     },
     methods: {
       switchName() {
         return this.name.split('').reverse().join('')
+      },
+      resetName() {
+        this.myName = 'Abracadabra'
+        this.$emit('nameWasReset', this.myName)
       }
+    },
+    created() {
+      eventBus.$on('ageWasEdited', age => {
+        this.userAge = age
+      })
     }
   }
 </script>
